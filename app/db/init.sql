ce564221af66858fc7643cfca032b8f40f7ac72a
AlekOmOm <alekellegard@protonmail.com>
2025-04-06 22:28:09 +0200

    Refactor Dockerfiles to improve structure and clarity; update database initialization script for PostgreSQL compatibility and enhanced user table definition.

--- app/db/init.sql
+++ app/db/init.sql
@@ -1,14 +1,36 @@
-CREATE TABLE search_results (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    title NVARCHAR(MAX) NOT NULL,
-    description NVARCHAR(MAX) NOT NULL,
-    url NVARCHAR(MAX) NOT NULL,
-    keywords NVARCHAR(MAX)
+-- db/init.sql 
+-- postgresql database initialization script
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
 );
 
--- Add some sample data
-INSERT INTO search_results (title, description, url, keywords)
+-- Create a default user, The password is 'password' (MD5 hashed)
+INSERT INTO users (username, email, password) 
+    VALUES ('admin', 'keamonk1@stud.kea.dk', '5f4dcc3b5aa765d61d8327deb882cf99');
+
+CREATE TABLE IF NOT EXISTS pages (
+    title TEXT PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    language TEXT NOT NULL CHECK(language IN ('en', 'da')) DEFAULT 'en',
+    last_updated TIMESTAMP,
+    content TEXT NOT NULL
+);
+
+-- Add search functionality data
+CREATE TABLE IF NOT EXISTS search_results (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    url TEXT NOT NULL
+);
+
+-- Sample search data
+INSERT INTO search_results (title, description, url)
 VALUES 
-('Example Search Result', 'This is a sample result description', 'https://example.com', '["example", "result"]'),
-('Another Result', 'This is another sample result description', 'https://another.com', '["another", "result"]'),
-('Third Result', 'This is the third sample result description', 'https://third.com', '["third", "result"]')
+    ('Example Result 1', 'This is a sample result description', 'https://example.com'),
+    ('Example Result 2', 'Another sample result', 'https://example.org');
